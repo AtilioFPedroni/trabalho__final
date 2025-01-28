6892c9301fbb430bff38bf96328a57ff20b35de1
atilio <atiliofpedroni@gmail.com>
2025-01-28 11:24:03 -0300

    adiciona precisão para as variáveis

--- app/main.f90
+++ app/main.f90
@@ -1,20 +1,21 @@
 program automato_celular
   use automato
   use rndgen_mod
+  use, intrinsic :: iso_fortran_env, only: sp => real32, dp => real64, i4 => int32, i8 => int64
   implicit none
 
-  integer :: n_regra, i, t, up(0:7)
-  integer :: sitio(n)
+  integer(kind=i4) :: n_regra, i, t, up(0:7)
+  integer(kind=i4) :: sitio(n)
   type(rndgen) :: rng
-  integer :: seed_value
-  integer :: unit_number
+  integer(kind=i4) :: seed_value
+  integer(kind=i4) :: unit_number
 
   unit_number = 10
 
   print *, "Digite o número da regra (0-255):"
   read*, n_regra
 
-  !verifica se o n_regra está dentro do intervalo
+  ! verifica se o n_regra está dentro do intervalo
   if (n_regra < 0 .or. n_regra > 255) then
     print *, "Número da regra inválido"
     stop
@@ -25,8 +26,7 @@ program automato_celular
 
   call rng%init(seed_value)
 
-  !inicia o automato com um único sítio "vivo" (1) no meio
-
+  ! inicia o automato com um único sítio "vivo" (1) no meio
   sitio = 0
   sitio(n/2) = 1
 
@@ -34,24 +34,24 @@ program automato_celular
 
   open(unit_number, file='automato.dat', status='replace')
 
-  do t = 1, n !para cada passo de tempo
+  do t = 1, n  ! para cada passo de tempo
     call print_sitio(sitio, unit_number)
-
     call passo(sitio, up, rng)
-
   end do
 
-  close(unit=unit_number)
+  close(unit_number)
 
   print *, "Dados gravados no arquivo automato.dat"
 
 contains
 
   subroutine print_sitio(sitio, unit_number)
-    integer, intent(in) :: sitio(n)
-    integer, intent(in) :: unit_number
+    use automato
+    implicit none
+    integer(kind=i4), intent(in) :: sitio(n)
+    integer(kind=i4), intent(in) :: unit_number
     character(len=1) :: char
-    integer :: i
+    integer(kind=i4) :: i
 
     do i = 1, n
       if (sitio(i) == 1) then
@@ -64,48 +64,6 @@ contains
 
     write(unit_number,*)
 
-    end subroutine print_sitio
+  end subroutine print_sitio
 
 end program automato_celular
-      
-      
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/trabalho__final.f90
+++ src/trabalho__final.f90
@@ -1,16 +1,16 @@
 module automato
-  use rndgen_mod
+  use, intrinsic :: iso_fortran_env, only: sp => real32, dp => real64, i4 => int32, i8 => int64
   implicit none
 
-  integer, parameter :: n= 50 ! Número de sítios no autômato
-  real, parameter :: p = 0.01 ! Probabilidade de inverter o estado
+  integer(kind=i4), parameter :: n = 50  ! Número de sítios no autômato
+  real(kind=dp), parameter :: p = 0.01_dp  ! Probabilidade de inverter o estado
 
   contains
     ! converte o número de regra para binário
     subroutine regra(n_regra, up)
-      integer, intent(in) :: n_regra
-      integer, intent(out) :: up(0:7)  !regra de 8 bits
-      integer :: i
+      integer(kind=i4), intent(in) :: n_regra
+      integer(kind=i4), intent(out) :: up(0:7)  !regra de 8 bits
+      integer(kind=i4) :: i
 
       do i = 0, 7
         up(i) = iand(ishft(n_regra, -i), 1)
@@ -20,38 +20,35 @@ module automato
     subroutine passo(sitio, up, rng)
       use rndgen_mod
       implicit none
-      integer, intent(inout) :: sitio(n)!itio é um vetor de N posições
-      integer, intent(in) :: up(0:7) 
-      type(rndgen), intent(inout) :: rng !gerador de números aleatórios
+      integer(kind=i4), intent(inout) :: sitio(n)  ! sitio é um vetor de N posições
+      integer(kind=i4), intent(in) :: up(0:7)
+      type(rndgen), intent(inout) :: rng ! gerador de números aleatórios
 
-      integer :: esquerda, direita, vizi, i
-      integer :: novo_sitio(n)
-      real :: random_number
+      integer(kind=i4) :: esquerda, direita, vizi, i
+      integer(kind=i4) :: novo_sitio(n)
+      real(kind=dp) :: random_number
 
       novo_sitio = 0
 
-      do i = 1, n!para cada sitio
-       ! calcula os vizinhos
+      do i = 1, n  ! para cada sitio
+        ! calcula os vizinhos
         esquerda = mod(i - 2 + n, n) + 1
-        
-        direita =  mod(i +0 + n, n) +1
+        direita = mod(i + 0 + n, n) + 1
 
-        ! calcula o valor do vizinho(3bits)
+        ! calcula o valor do vizinho (3 bits)
         vizi = sitio(esquerda) * 4 + sitio(i) * 2 + sitio(direita)
 
-        !aplicar a regra para determinar o novo valor do sitio
+        ! aplicar a regra para determinar o novo valor do sitio
         novo_sitio(i) = up(vizi)
 
-        !adiciona aleatoridade: inverte o valor do sitio com probabilidade p
+        ! adiciona aleatoriedade: inverte o valor do sitio com probabilidade p
         random_number = rng%rnd()
         if (random_number < p) then
-          novo_sitio(i) = 1 - novo_sitio(i) !inverte o valor
+          novo_sitio(i) = 1 - novo_sitio(i)  ! inverte o valor
         end if
       end do
 
       sitio = novo_sitio
 
-      end subroutine passo
-
-   end module automato
-    
+    end subroutine passo
+  end module automato
